a48e8a2a9ef0808ecad31d77297a52e1203e58d7
heshengpku <hesheng.phy@pku.edu.cn>
2017-10-21 13:32:47 +0800

    Change tempBlocks to tempBlock in isValidCain function

--- main.js
+++ main.js
@@ -180,10 +180,10 @@ var isValidChain = (blockchainToValidate) => {
     if (JSON.stringify(blockchainToValidate[0]) !== JSON.stringify(getGenesisBlock())) {
         return false;
     }
-    var tempBlocks = [blockchainToValidate[0]];
+    var tempBlock = blockchainToValidate[0];
     for (var i = 1; i < blockchainToValidate.length; i++) {
-        if (isValidNewBlock(blockchainToValidate[i], tempBlocks[i - 1])) {
-            tempBlocks.push(blockchainToValidate[i]);
+        if (isValidNewBlock(blockchainToValidate[i], tempBlock)) {
+            tempBlock = blockchainToValidate[i];
         } else {
             return false;
         }
